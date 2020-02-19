cc541d8bf0d1e798994fd32e0a209cf5d0fd2392
15801001455 <15801001455@126.com>
2020-02-19 23:08:48 +0800

    增加熔断器，但是两种方式实现感觉都有问题，具体要问问同事

--- project/hello-spring-cloud/hello-spring-cloud-web-admin-feign/src/main/java/com/funtl/hello/spring/cloud/web/admin/feign/service/AdminService.java
+++ project/hello-spring-cloud/hello-spring-cloud-web-admin-feign/src/main/java/com/funtl/hello/spring/cloud/web/admin/feign/service/AdminService.java
@@ -1,12 +1,13 @@
 package com.funtl.hello.spring.cloud.web.admin.feign.service;
 
+import com.funtl.hello.spring.cloud.web.admin.feign.service.hystrix.AdminServiceHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 //通过 @FeignClient("服务名") 注解来指定调用哪个服务。代码如下：
-@FeignClient(value = "hello-spring-cloud-service-admin")
+@FeignClient(value = "hello-spring-cloud-service-admin", fallback = AdminServiceHystrix.class)
 public interface AdminService {
     //service层要加RequestMapping注解，因为这里才是真正调用eureka服务的地方,很容易写错了
     @RequestMapping(value = "hi", method = RequestMethod.GET)
